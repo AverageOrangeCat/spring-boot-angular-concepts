d51f64db6b869c65a80abc44238a76e38b946b16
AverageOrangeCat <74761380+AverageOrangeCat@users.noreply.github.com>
2024-10-06 01:16:02 +0200

    Implement basic background worker concept

--- backend/src/main/java/spring/boot/angular/concepts/backend/Program.java
+++ backend/src/main/java/spring/boot/angular/concepts/backend/Program.java
@@ -1,13 +1,25 @@
 package spring.boot.angular.concepts.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+import spring.boot.angular.concepts.backend.workers.BackgroundWorker;
 
 @SpringBootApplication
 public class Program {
 
+    @Autowired
+    private BackgroundWorker backgroundWorker;
+
     public static void main(String[] args) {
         SpringApplication.run(Program.class, args);
+    }
+
+    @PostConstruct
+    public void startWorkers() {
+        backgroundWorker.start();
     }
 
 }
---
+++ backend/src/main/java/spring/boot/angular/concepts/backend/workers/BackgroundWorker.java
@@ -0,0 +1,27 @@
+package spring.boot.angular.concepts.backend.workers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BackgroundWorker extends Thread {
+
+    private Logger logger = LoggerFactory.getLogger(BackgroundWorker.class);
+
+    @Override
+    public void run() {
+        while (true) {
+
+            logger.debug("Hello from 'BackgroundWorker'");
+
+            try {
+                Thread.sleep(10_000);
+
+            } catch (InterruptedException exception) {
+                interrupt();
+            }
+        }
+    }
+
+}
