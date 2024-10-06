adaa67eed0d91b360121719f82e1cd9248b1d3c3
AverageOrangeCat <74761380+AverageOrangeCat@users.noreply.github.com>
2024-10-06 18:58:39 +0200

    Remove unused frontend resource loader

--- backend/src/main/java/spring/boot/angular/concepts/backend/WebConfiguration.java
+++ backend/src/main/java/spring/boot/angular/concepts/backend/WebConfiguration.java
@@ -1,17 +1,9 @@
 package spring.boot.angular.concepts.backend;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-            .addResourceHandler("/**")
-            .addResourceLocations("classpath:/webapp/browser/");
-    }
 
 }
